73b7f25c1f32c89904f5ce3e2975df5cc9e64b8c
Ivan <ivan.gonzalez@cceo.com.mx>
2020-08-14 15:31:10 -0500

    contador added

---
+++ contador/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//Importaciones que nosotros hayamos escrito
+//import 'src/app.dart';
+import 'package:contador/src/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
